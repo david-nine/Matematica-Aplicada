ab3438d280072443b3f21c98c7b4b44469979c4f
David Hildebrandt <davihildebran@gmail.com>
2021-05-27 23:54:55 -0300

    Finalizacao do trabalho calculadora

--- trabalhoCalculadora.py
+++ trabalhoCalculadora.py
@@ -1,21 +1,23 @@
 import numpy as np
 
 class Interface():
-    DICIONARIO_SINAIS = {"a": "+", "b" : "-", "c" : "x", "d" : "÷"}
 
-    def imprimirManualFracao(sinal):
-        print(" a     c")
-        print("--- ", DICIONARIO_SINAIS[sinal], " ---")
-        print(" b     d")
+    def __init__(self):
+        self.DICIONARIO_SINAIS = {"a": "+", "b" : "-", "c" : "x", "d" : "÷"}
 
-    def pedirFracoes():
+    def imprimir_manual_fracao(self, sinal):
+        print(" a       c")
+        print("--- ", self.DICIONARIO_SINAIS[sinal], " ---")
+        print(" b       d")
+
+    def pedir_fracoes(self):
         a = float(input("Digite a: "))
         b = float(input("Digite b: "))
         c = float(input("Digite c: "))
         d = float(input("Digite d: "))
         return (a, b, c, d)
 
-    def instrucoesMatriz():
+    def instrucoes_matriz(self):
         print("Digite a matriz separando os valores por virgula e as +\
               \nlinhas por colchetes como no exemplo abaixo: ")
         print("[1, 2, 3], [4, 5, 6], [7, 8, 9]")
@@ -23,11 +25,11 @@ class Interface():
         print("linha 2 = [4, 5, 6]")
         print("linha 3 = [7, 8, 9]")
 
-    def pedirMatriz(letra):
-        matriz = eval(input("Digite a matriz ", letra, " 3x3: "))
+    def pedir_matriz(self):
+        matriz = eval(input("Digite a matriz 3x3: "))
         return matriz
 
-    def mostrarOpcoes():
+    def mostrar_opcoes(self):
         print("a. Soma de duas frações")
         print("b. Subtração de duas frações")
         print("c. Multiplicação de duas frações")
@@ -37,94 +39,104 @@ class Interface():
         opcao = input("Digite a letra minuscula da opcao: ")
         return opcao
 
-    def mostrarResultadoFracao(resultado):
+    def mostrar_resultado(self, resultado):
         print(resultado)
+
+    def opcao_invalida(self):
+        print("opcao inválida!")
         
 
 class Main():
 
-    def inicializar():
-        opcao = Inteface.mostraOpcoes()
-        if opcao == ("a" or "b" or "c" or "d"):
-            Interface.imprimirManualFracao(opcao)
-            valores = Interface.pedirFracoes()
-            fracoes = Calculadora.calcularFracoes(valores)
-            resultado = self.calculoFracoes(opcao, fracoes)
-            Interface.mostrarResultado(resultado)
+    def __init__(self):
+        self.interface = Interface()
+        self.calculadora = Claculadora()
+
+    def inicializar(self):
+        opcao = self.interface.mostrar_opcoes()
+        if opcao == "a" or opcao == "b" or opcao == "c" or opcao == "d":
+            self.interface.imprimir_manual_fracao(opcao)
+            valores = self.interface.pedir_fracoes()
+            fracoes = self.calculadora.calcular_fracoes(valores)
+            resultado = self.calculo_fracoes(opcao, fracoes)
+            self.interface.mostrar_resultado(resultado)
 
         elif opcao == "e" or opcao == "f":
-            Interface.instrucoesMatriz()
-            matrizA = Interface.pedirMatriz("A")
-            matrizB = Interface.pedirMatriz("B")
-            np.array(matrizA)
-            np.array(matrizB)
-            resultado = self.calculoMatriz(opcao, matrizA, matrizB)
-            Interface.mostrarResultado(resultado)
+            self.interface.instrucoes_matriz()
+            matriz_a = self.interface.pedir_matriz()
+            matriz_b = self.interface.pedir_matriz()
+            np.array(matriz_a)
+            np.array(matriz_b)
+            resultado = self.calculo_matriz(opcao, matriz_a, matriz_b)
+            self.interface.mostrar_resultado(resultado)
         else:
-            Interface.opcaoInvalida()
+            self.interface.opcao_invalida()
     
-    def calculoFracoes(opcao, fracoes):
-        fracaoA = fracoes[0]
-        fracaoB = fracoes[1]
+    def calculo_fracoes(self, opcao, fracoes):
+        fracao_a = fracoes[0]
+        fracao_b = fracoes[1]
         if opcao == "a":    
-            resultado = Calculadora.somarFracoes(fracaoA, fracaoB)
+            resultado = self.calculadora.somar_fracoes(fracao_a, fracao_b)
         elif opcao == "b":
-            resultado = Calculadora.subtrairFracoes(fracaoA, fracaoB)
+            resultado = self.calculadora.subtrair_fracoes(fracao_a, fracao_b)
         elif opcao == "c":
-            resultado = Calculadora.multiplicarFracoes(fracaoA, fracaoB)
+            resultado = self.calculadora.multiplicar_fracoes(fracao_a, fracao_b)
         else:
-            resultado = Calculadora.dividirFracoes(fracaoA, fracaoB)
+            resultado = self.calculadora.dividir_fracoes(fracao_a, fracao_b)
         return resultado
 
-    def calculoMatriz(opcao, matrizA, matrizB):
-        if !(Utils.validaMatriz(matrizA) and Utils.validaMatriz(matriz)):
-            Interface.matrizInvalida()
+    def calculo_matriz(self, opcao, matriz_a, matriz_b):
+        if not(Utils().valida_matriz(matriz_a) and Utils().valida_matriz(matriz_b)):
+            self.interface.matriz_invalida()
         else:
             if opcao == "e":
-                return Calculadora.somarMatrizes(matrizA, matrizB)
+                return self.calculadora.somar_matrizes(matriz_a, matriz_b)
             else:
-                return Calculadora.multiplicarMatrizes(matrizA, matrizB)
+                return self.calculadora.multiplicar_matrizes(matriz_a, matriz_b)
 
 class Claculadora():
 
-    def calcularFracoes(tuplaValores):
-        fracaoA = tuplaValores[0] / tuplaValores[1]    
-        fracaoB = tuplaValores[2] / tuplaValores[3]
-        return (fracaoA, fracaoB)
+    def calcular_fracoes(self, tupla_valores):
+        fracao_a = tupla_valores[0] / tupla_valores[1]    
+        fracao_b = tupla_valores[2] / tupla_valores[3]
+        return (fracao_a, fracao_b)
 
-    def somarFracoes(facaoA, fracaoB):
-        return fracaoA + fracaoB
+    def somar_fracoes(self, fracao_a, fracao_b):
+        return fracao_a + fracao_b
 
-    def subtrairFracoes(facaoA, fracaoB):
-        return fracaoA - fracaoB
+    def subtrair_fracoes(self, fracao_a, fracao_b):
+        return fracao_a - fracao_b
     
-    def multiplicarFracoes(facaoA, fracaoB):
-        return fracaoA * fracaoB
+    def multiplicar_fracoes(self, fracao_a, fracao_b):
+        return fracao_a * fracao_b
 
-    def dividirFracoes(facaoA, fracaoB):
-        return fracaoA / fracaoB
+    def dividir_fracoes(self, fracao_a, fracao_b):
+        return fracao_a / fracao_b
 
-    def multiplicarMatrizes(matrizA, matrizB):
-        return np.dot(matrizA, matrizB) 
+    def multiplicar_matrizes(self, matriz_a, matriz_b):
+        return np.dot(matriz_a, matriz_b) 
 
-    def somarMatrizes(matrizA, matrizB):
-        return np.add(matrizA, matrizB)
+    def somar_matrizes(self, matriz_a, matriz_b):
+        return np.add(matriz_a, matriz_b)
 
 class Utils():
 
-    def validaTamanhoLinhaMatriz(matriz):
+    def valida_tamanho_linha_matriz(self, matriz):
         if len(matriz) == 3:
             return True
         return False
 
-    def validaTamanhoColunaMatriz(matriz):
-        for linha in matriz():
+    def valida_tamanho_coluna_matriz(self, matriz):
+        for linha in matriz:
             if len(linha) != 3:
                 return False
         return True
 
-    def validaMatriz(matriz):
-        if validaTamanhoLinhaMatriz:
-            if validaTamanhoColunaMatriz:
+    def valida_matriz(self, matriz):
+        if self.valida_tamanho_linha_matriz(matriz) and  self.valida_tamanho_coluna_matriz(matriz):
                 return True
         return False
+
+if __name__ == "__main__":
+    main = Main()
+    main.inicializar()
